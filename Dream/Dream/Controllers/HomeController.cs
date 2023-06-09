0356b18feeb58ece3aaa60b3839ae5ba525ce647
Mayerli Azucena Montalvo <87994175+Mayerli2003@users.noreply.github.com>
2023-06-08 23:59:36 -0600

    Contacto

--- Dream/Dream/Controllers/HomeController.cs
+++ Dream/Dream/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Dream.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "______________________________Contacto______________________________";
 
             return View();
         }
